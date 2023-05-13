039ec911117b472907d032c92f8082eb80494c1d
eric <ericzhao96@hotmail.com>
2023-05-13 08:02:53 +0800

    feat: clean code

--- scripts/clear.js
+++ scripts/clear.js
@@ -3,6 +3,8 @@ const path = require('path')
 
 const BUILD_DIR = path.resolve(__dirname, '../build')
 
-fs.rmdirSync(BUILD_DIR, { recursive: true })
+if (fs.existsSync(BUILD_DIR)) {
+    fs.rmdirSync(BUILD_DIR, { recursive: true })
+}
 
 console.log(`removed ${BUILD_DIR}`)
